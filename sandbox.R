25bd41ca90a30c6c2fd1e2fa007d244918cd0ef0
geraud <geraudster@gmail.com>
2015-06-21 23:11:12 +0200

    add traincontrol

--- sandbox.R
+++ sandbox.R
@@ -1,15 +1,15 @@
-train <- read.csv('trainset.csv', header = TRUE)
+train <- read.csv('trainset.csv', header = TRUE, row.names = 1)
 train$Made.Donation.in.March.2007 <- factor(train$Made.Donation.in.March.2007)
 str(train)
 dim(train)
 
-validation <- read.csv('testset.csv', header = TRUE)
+validation <- read.csv('testset.csv', header = TRUE, row.names = 1)
 
 summary(train)
 pairs(train)
 library(ggplot2)
 library(reshape)
-longtrain <- melt(train, id=c('X', 'Made.Donation.in.March.2007'))
+longtrain <- melt(train, id=c('Made.Donation.in.March.2007'))
 qplot(Made.Donation.in.March.2007, value,
       geom = 'boxplot',
       data = longtrain,
@@ -23,18 +23,17 @@ inTrain <- createDataPartition(y=train$Made.Donation.in.March.2007, p=0.8, list=
 training <- train[inTrain,]
 testing <- train[-inTrain,]
 tc <- trainControl(classProbs = TRUE, method = "repeatedcv",
-                   number = 10,
-                   repeats = 10)
+                   number = 2,
+                   repeats = 2)
 glmModel <- testModel(Made.Donation.in.March.2007 ~ ., as.data.frame(training),
                         as.data.frame(testing),
                         'Made.Donation.in.March.2007',
-                        'glm',
-                        trainControl = tc)
+                        'glm', trControl = tc)
 
 rpartModel <- testModel(Made.Donation.in.March.2007 ~ ., as.data.frame(training),
                         as.data.frame(testing),
                         'Made.Donation.in.March.2007',
-                        'rpart')
+                        'rpart', trControl = tc, classProbs = TRUE)
 
 rf <- testModel(Made.Donation.in.March.2007 ~ ., training, testing,
                 'Made.Donation.in.March.2007',
@@ -45,3 +44,12 @@ rpart.test <- rpart(Made.Donation.in.March.2007 ~ ., training)
 predictions <- predict(rpart.test, newdata = testing)
 table(testing$Made.Donation.in.March.2007, predictions[,1] > 0.5)
 
+
+resamp <- resamples(
+    list(GLM=glmModel$fit,
+         RPART=rpartModel$fit,
+         RF=rf$fit))
+summary(resamp)
+bwplot(resamp)
+
+head(rpartModel$predictions)
